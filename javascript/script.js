b41b3c4393e7eeddbe2c165bc96f701087acf5df
Shubham Handergule <shubhamhandergule77@gmail.com>
2022-11-22 19:28:49 +0530

    new reporting page

--- javascript/script.js
+++ javascript/script.js
@@ -13,6 +13,14 @@ if (sidebar) {
     sidebar.classList.toggle("close");
   });
 }
+// ---------date picker
+$(function() {
+  $('input[name="daterangedata"]').daterangepicker({
+    opens: 'left'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+});
 $(document).ready(function (){
   $("#mobile-toggle-bar").click(function () {
     $("#sidebar").removeClass("sidebarClose")
@@ -397,3 +405,44 @@ $("#editorimg").on("click", function () {
     }
   }
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+  var demo1 = new BVSelect({
+    selector: "#selectbox",
+    searchbox: false,
+    offset: false
+  });
+  var demo2 = new BVSelect({
+    selector: "#selectbox2",
+    searchbox: false,
+    search_autofocus: true,
+    // offset: true,
+    width: "100%",
+    placeholder: "Select Option",
+    search_placeholder: "Search...",
+    // breakpoint: 750
+  });
+  var demo3 = new BVSelect({
+    selector: "#selectbox3",
+    searchbox: true,
+    offset: true
+  });
+  var demo4 = new BVSelect({
+    selector: "#selectbox4",
+    searchbox: true,
+    offset: true
+  });
+  var demo5 = new BVSelect({
+    selector: "#selectbox5",
+    searchbox: true,
+    offset: true
+  });
+  var demo6 = new BVSelect({
+    selector: "#selectbox6",
+    searchbox: false,
+    search_autofocus: false,
+    offset: true,
+    width: "100%",
+    search_placeholder: "Search..."
+  });
+});

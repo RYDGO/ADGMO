56c315991513d6efd67c3a8ab0cfea2c7d85aa71
Shubham Handergule <shubhamhandergule77@gmail.com>
2022-12-13 14:46:00 +0530

    pagination box design

--- javascript/tableDropdown.js
+++ javascript/tableDropdown.js
@@ -220,8 +220,10 @@ items.slice(perPage).hide();
 $('#pagination-container').pagination({
   items: numItems,
   itemsOnPage: perPage,
-  prevText: "&laquo;",
-  nextText: "&raquo;",
+  prevText: "&lt;",
+  nextText: "&gt;",
+  displayedPages: 0,
+  edges: 0,
   onPageClick: function (pageNumber) {
     var showFrom = perPage * (pageNumber - 1);
     var showTo = showFrom + perPage;
